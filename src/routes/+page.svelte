53c7302271fb0585ead6a0aca0f83ba863228e97
nruedebusch <140553125+nruedebusch@users.noreply.github.com>
2023-07-31 11:38:17 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@
   const addToDatabase = () => {
     if (inputValue.trim() !== "") {
       push(moviesInDB, inputValue);
-      console.log(`${inputValue} added to database`);
+      console.log(`${inputValue} added to atabase`);
       movies = [...movies, inputValue];
       inputValue = "";
     }
